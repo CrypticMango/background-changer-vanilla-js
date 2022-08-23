7c2f511fc37281513518b8fa6104471f5c66bd2e
Lisa Allen <79679706+CrypticMango@users.noreply.github.com>
2022-08-23 14:48:08 -0600

    added function for changing backgorund
    -added function for rgb color

--- app.js
+++ app.js
@@ -1,7 +1,19 @@
-const changeBackground = document.getElementById('color-button');
-const changeRgb = document.getElementById('color-text');
+const change = document.getElementById("button");
+const color = document.getElementById("color-text");
 const body = document.body;
 
-function buttonClick() {
-	alert("hello");
+change.addEventListener("click", changeBackground);
+
+function changeBackground() {
+	const color1 = randomRbg();
+	const color2 = randomRbg();
+	const color3 = randomRbg();
+
+	const colorString = `rgb(${color1}, ${color2}, ${color3})`;
+
+	console.log(colorString);
+}
+
+function randomRbg() {
+	return Math.floor(Math.random() * 256);
 }
